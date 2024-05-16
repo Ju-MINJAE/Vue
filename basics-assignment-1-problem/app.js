c1581e5720ff27e12126ab7c68bf3787cbeff302
Ju-MINJAE <juminjae98@gmail.com>
2024-05-16 15:47:10 +0900

    basics-assignment
    데이터 바인딩 실습

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: 'JuMinJae',
+      age: 27,
+      imageUrl:
+        'https://i.namu.wiki/i/d1A_wD4kuLHmOOFqJdVlOXVt1TWA9NfNt_HA0CS0Y_N0zayUAX8olMuv7odG2FiDLDQZIRBqbPQwBSArXfEJlQ.webp',
+    };
+  },
+  methods: {
+    calculateAge() {
+      return this.age + 5;
+    },
+    calculateRandom() {
+      return Math.random();
+    },
+  },
+});
+
+app.mount('#assignment');
