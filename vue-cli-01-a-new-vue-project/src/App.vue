34db2699a75cf9e53594af3f552a1d73a80f182f
Ju-MINJAE <juminjae98@gmail.com>
2024-05-24 18:45:36 +0900

    Vue app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Kang',
+          name: 'Kane HJ',
+          phone: '010-1234-1234',
+          email: 'vue@study.com',
+        },
+        {
+          id: 'Hwang',
+          name: 'Hwang JW',
+          phone: '010-5666-6666',
+          email: 'u@study.com',
+        },
+      ],
+    };
+  },
+};
+</script>
