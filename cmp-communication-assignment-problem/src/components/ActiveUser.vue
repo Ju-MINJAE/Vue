2915e2a84943a67586f014ce3f45abb268e9159b
Ju-MINJAE <juminjae98@gmail.com>
2024-05-31 21:06:36 +0900

    Create ActiveUser.vue

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h2>JuMinJae</h2>
+    <h3>27 Years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+}
+</style>
