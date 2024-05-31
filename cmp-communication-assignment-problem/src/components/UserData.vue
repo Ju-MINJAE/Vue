506eb101c57287f3e59f3941520bf8ccadaf8505
Ju-MINJAE <juminjae98@gmail.com>
2024-05-31 21:06:39 +0900

    Create UserData.vue

---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <input type="text" placeholder="Your name" />
+    <input type="text" placeholder="Your age" />
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
