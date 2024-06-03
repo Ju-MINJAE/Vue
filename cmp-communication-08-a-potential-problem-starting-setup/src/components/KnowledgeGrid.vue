e1bdd8e36beb668a5ffbb35370cb9ffbc63fecdb
Ju-MINJAE <juminjae98@gmail.com>
2024-06-03 15:28:10 +0900

    provide / inject

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // inject(주입) : 하위 컴포넌트에서 제공된 props를 주입
+  // 직계 부모보다 더 상위의 부모로부터도 데이터를 주입을 받을 수 있음
+  inject: ['topics'],
+  emits: ['select-topic'],
+};
+</script>
