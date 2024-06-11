8790ecaae1013c8bc3687f69bd78805212497fd1
Ju-MINJAE <juminjae98@gmail.com>
2024-06-11 15:52:41 +0900

    fetch(GET)

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,15 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found.
+      </p>
+      <ul v-else-if="!isLoading && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +27,48 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+    };
+  },
+
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch(
+        'https://vue-http-demo-3385d-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json',
+        {
+          method: 'GET',
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  // 이 컴포넌트가 마운트 된 이후 호출
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
