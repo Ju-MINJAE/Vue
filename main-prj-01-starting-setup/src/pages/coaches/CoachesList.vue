e2d3575c5b74cb530cfd7e299e9128f6bbfcce52
Ju-MINJAE <juminjae98@gmail.com>
2024-06-28 22:41:04 +0900

    CoahForm, CoachRegistation

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,5 @@
 <template>
-  <coach-filter></coach-filter>
+  <coach-filter @change-filter="setFilters"></coach-filter>
   <section>
     <base-card>
       <div class="controls">
@@ -28,12 +28,35 @@ import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
   components: { CoachItem, CoachFilter },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
   computed: {
     fillteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend'))
+          return true;
+        if (this.activeFilters.backend && coach.areas.includes('backend'))
+          return true;
+        if (this.activeFilters.career && coach.areas.includes('career'))
+          return true;
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+  methods: {
+    setFilters(updateFilters) {
+      this.activeFilters = updateFilters;
     },
   },
 };
